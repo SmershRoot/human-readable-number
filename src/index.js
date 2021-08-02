b013bc9812fa0fb9e31086f5e6fcab1887013b3e
Smersh <Smrsh.Root@gmail.com>
2021-08-02 21:29:47 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const lessTwenty = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen'
+    ];
+    const multiplesTen = [
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety'
+    ];
+
+    let thousand = ~~(number / 1000);
+    let hundred = ~~((number % 1000) / 100);
+    let ten = ~~(((number % 1000) % 100) / 10);
+    let unit = (((number % 1000) % 100) % 10);
+
+    let result = "";
+
+    if (number === 0) {
+        return "zero";
+    }
+
+    if (thousand > 0) {
+        result += toReadable(thousand) + ' thousand ';
+    }
+    if (hundred > 0) {
+        result += lessTwenty[hundred - 1] + ' hundred ';
+    }
+    if (ten > 1) {
+        result += multiplesTen[ten - 2] + ' ';
+    } else {
+        unit += 10 * ten;
+    }
+    if (unit > 0) {
+        result += lessTwenty[unit - 1];
+    }
+    return result.trim();
 }
